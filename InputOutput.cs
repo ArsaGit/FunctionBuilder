e37660908442dbf62a41ffa233afaaeda73725b9
l <l>
2021-06-14 23:58:26 +0500

    -написал метод считывания данных -позже доделать метод записи данных в текстовый файл

--- InputOutput.cs
+++ InputOutput.cs
@@ -1,11 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace FunctionBuilder
 {
-	class InputOutput
+	public class InputOutput
 	{
+		//создаёт относительный путь к фалйу
+		private string GetPathToFile(string fileName)
+		{
+			string filePath = Environment.CurrentDirectory;
+			filePath = filePath.Substring(0, filePath.IndexOf("bin")) + fileName;
+			return filePath;
+		}
 
+		//считывает данные из файла и возвращает массив строк
+		public string[] ReadFile()
+		{
+			string[] input = new string[4];
+			string path = GetPathToFile("input.txt");
+			int i = 0;
+			foreach(string line in File.ReadLines(path))
+			{
+				input[i] = line;
+				i++;
+			}
+			return input;
+		}
+
+		//В работе
+		public void WriteInFile()
+		{
+			string path = GetPathToFile("output.txt");
+			using (StreamWriter sw = new StreamWriter(path))
+			{
+				//sw.Write(content);	//запись данных
+			}
+		}
+
+		
 	}
 }
