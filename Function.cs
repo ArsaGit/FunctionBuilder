29923f84287acd651e46ab6564260efb7eff5612
l <l>
2021-06-19 20:25:57 +0500

    added constructor for Function

--- Function.cs
+++ Function.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace FunctionBuilder
 {
-	class Function
+	public class Function
 	{
+		private string expression;
+		private double x0, x1, step;
+
+		public Function(string[] functionData)
+		{
+			expression = functionData[0];
+			x0 = ConvertToDouble(functionData[1]);
+			x1 = ConvertToDouble(functionData[2]);
+			step = ConvertToDouble(functionData[3]);
+		}
+
+		private double ConvertToDouble(string strNumber)
+		{
+			return double.Parse(strNumber);
+		}
 	}
 }
