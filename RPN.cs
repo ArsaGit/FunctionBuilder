22868c3bea2356dd61b0c8c03b7ba849d4e70c7e
l <l>
2021-06-19 23:52:22 +0500

    init commit

--- RPN.cs
+++ RPN.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace FunctionBuilder
 {
-	class RPN
+	public class RPN
 	{
+		public List<object> ParseExpression(string strExpression)
+		{
+			List<object> list = new List<object>();
+			int i = 0;
+
+			while(i < strExpression.Length)
+			{
+				if(char.IsDigit(strExpression[i]))
+				{
+
+				}
+				else if(char)
+
+			}
+		}
 	}
 }
